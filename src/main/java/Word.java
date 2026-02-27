66cfe1a1a9a60810fd43f2f58a4ccff663ad30cb
[ashtoncook67] <[226217@andrews.esc18.net]>
2026-02-27 10:15:56 -0600

    Sort By Vowels Complete

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -13,28 +13,49 @@ public class Word implements Comparable<Word>
 		word = s;
 	}
 
-	public int compareTo( Word rhs )
-	{	
-		if(word.equals(rhs.word)){
-			return 0;
-		}
-		if(word.length() < rhs.word.length()){
-			return 1;
-		}
-		if(word.length() > rhs.word.length()){
-			return -1;
-		}
-		if(word.length() == rhs.word.length()){
-			for(int i = 0; i < word.length(); i++){
-				if(word.charAt(i) < rhs.word.charAt(i)){
-					return 1;
-				}
-				if(word.charAt(i) > rhs.word.charAt(i)){
-					return -1;
-				}
+	public int numOfVowels(String words){
+		char disChar = 'A';
+		int numOfVow = 0;
+		for(int i = 0; i < words.length(); i++){
+			disChar = words.charAt(i);
+			if(disChar == 'a' || disChar == 'e' || disChar == 'i' || disChar == 'o' || disChar == 'u'){
+				numOfVow = numOfVow + 1;
 			}
 		}
+		return numOfVow;
+	}
+
+	public boolean alphabetize(String word1, String word2, int place){
+		if(word1.charAt(place) < word2.charAt(place)){
+			return true;
+		}
+		if(word1.charAt(place) > word2.charAt(place)){
+			return false;
+		}
+		return alphabetize(word1, word2, place++);
+	}
+	
+	public int compareTo( Word rhs )
+	{	
+		if(numOfVowels(word) == numOfVowels(rhs.word)){
+			if(word.equals(rhs.word)){
+			return 0;
+			}
+			if(alphabetize(word, rhs.word, 0)){
+				return 1;
+			}else{
+				return -1;
+			}
+		}
+		if(numOfVowels(word) > numOfVowels(rhs.word)){
+			return -1;
+		}
+		if(numOfVowels(word) < numOfVowels(rhs.word)){
+			return 1;
+		}
+		
 		return -2;
+		
 	}
 
 	public String toString()
